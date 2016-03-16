c586af4938c768985344c02d21d2e4126398fc75
ntr <ntr@ihfood.dk>
2016-03-16 15:08:44 +0100

    Add Unsafe versions of Anderman and Illyriad and add more bytes copied test cases.

--- src/DotNetCross.Memory.Copies.Benchmarks/CopiesBenchmark.cs
+++ src/DotNetCross.Memory.Copies.Benchmarks/CopiesBenchmark.cs
@@ -15,19 +15,19 @@ namespace DotNetCross.Memory.Copies.Benchmarks
             public Config()
             {
                 Add(Job.AllJits.Select(j => j.WithLaunchCount(1).WithWarmupCount(1).WithTargetCount(5)).ToArray());
+                //Add(Job.RyuJitX64.WithLaunchCount(1).WithWarmupCount(1).WithTargetCount(5));
                 Add(StatisticColumn.AllStatistics);
             }
         }
 
+        //[Params(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)]
         [Params(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
             33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
             65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96,
-            508, 509, 510, 511, 512, 513, 512 + 31, 512 + 32, 512 + 35, 512 + 64,
-            1024,
-            4096,
-            8512)]
+            128, 256, 508, 509, 510, 511, 512, 513, 512 + 31, 512 + 32, 512 + 35, 512 + 64,
+            1024, 2048, 4096, 2 * 4096, 8512)]
         public int BytesCopied = 0;
-        [Params(0)]
+        //[Params(0)]
         public int Index = 0;
 
         const int BufferSize = 16 * 1024 * 1024;
@@ -59,8 +59,21 @@ namespace DotNetCross.Memory.Copies.Benchmarks
         }
 
         [Benchmark]
+        public unsafe void UnsafeIllyriadVectorizedCopy()
+        {
+            UnsafeIllyriad.VectorizedCopy(bufferFrom, Index, bufferTo, Index, BytesCopied);
+        }
+
+        [Benchmark]
+        public unsafe void UnsafeAndermanVectorizedCopy()
+        {
+            UnsafeAnderman.VectorizedCopy(bufferFrom, Index, bufferTo, Index, BytesCopied);
+        }
+
+        [Benchmark]
         public unsafe void UnsafeCopyBlock()
         {
+            // No checks
             fixed (byte* src = bufferFrom)
             fixed (byte* dst = bufferFrom)
             {
---
+++ src/DotNetCross.Memory.Copies.Benchmarks/UnsafeIllyriad.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Numerics;
+
+namespace DotNetCross.Memory.Copies.Benchmarks
+{
+    // Based on: https://github.com/IllyriadGames/ByteArrayExtensions/blob/master/src/IllyriadGames.ByteArrayExtensions/VectorizedCopyExtension.cs
+    public static class UnsafeIllyriad
+    {
+        // Will be Jit'd to consts https://github.com/dotnet/coreclr/issues/1079
+        private static readonly int _vectorSpan = Vector<byte>.Count;
+        private static readonly int _vectorSpan2 = Vector<byte>.Count + Vector<byte>.Count;
+        private static readonly int _vectorSpan3 = Vector<byte>.Count + Vector<byte>.Count + Vector<byte>.Count;
+        private static readonly int _vectorSpan4 = Vector<byte>.Count + Vector<byte>.Count + Vector<byte>.Count + Vector<byte>.Count;
+
+        private const int _longSpan = sizeof(long);
+        private const int _longSpan2 = sizeof(long) + sizeof(long);
+        private const int _longSpan3 = sizeof(long) + sizeof(long) + sizeof(long);
+        private const int _intSpan = sizeof(int);
+
+        /// <summary>
+        /// Copies a specified number of bytes from a source array starting at a particular
+        /// offset to a destination array starting at a particular offset, not safe for overlapping data.
+        /// </summary>
+        /// <param name="src">The source buffer</param>
+        /// <param name="srcOffset">The zero-based byte offset into src</param>
+        /// <param name="dst">The destination buffer</param>
+        /// <param name="dstOffset">The zero-based byte offset into dst</param>
+        /// <param name="count">The number of bytes to copy</param>
+        /// <exception cref="ArgumentNullException"><paramref name="src"/> or <paramref name="dst"/> is null</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="srcOffset"/>, <paramref name="dstOffset"/>, or <paramref name="count"/> is less than 0</exception>
+        /// <exception cref="ArgumentException">
+        /// The number of bytes in src is less
+        /// than srcOffset plus count.-or- The number of bytes in dst is less than dstOffset
+        /// plus count.
+        /// </exception>
+        /// <remarks>
+        /// Code must be optimized, in release mode and <see cref="Vector"/>.IsHardwareAccelerated must be true for the performance benefits.
+        /// </remarks>
+        public unsafe static void VectorizedCopy(byte[] src, int srcOffset, byte[] dst, int dstOffset, int count)
+        {
+            if (src == null) throw new ArgumentNullException(nameof(src));
+            if (dst == null) throw new ArgumentNullException(nameof(dst));
+            if (count < 0 || srcOffset < 0 || dstOffset < 0) throw new ArgumentOutOfRangeException(nameof(count));
+            if (count == 0) return;
+            if (srcOffset + count > src.Length) throw new ArgumentException(nameof(src));
+            if (dstOffset + count > dst.Length) throw new ArgumentException(nameof(dst));
+
+            fixed (byte* srcOrigin = src)
+            fixed (byte* dstOrigin = dst)
+            {
+                var srcPtr = srcOrigin + srcOffset;
+                var dstPtr = dstOrigin + dstOffset;
+
+#if !DEBUG
+                // Tests need to check even if IsHardwareAccelerated == false
+                // Check will be Jitted away https://github.com/dotnet/coreclr/issues/1079
+                if (Vector.IsHardwareAccelerated && count <= 512 + 64)
+                {
+#endif
+                    while (count >= _vectorSpan4)
+                    {
+                        Unsafe.Write(dstPtr, Unsafe.Read<Vector<byte>>(srcPtr));
+                        Unsafe.Write(dstPtr + _vectorSpan, Unsafe.Read<Vector<byte>>(srcPtr + _vectorSpan));
+                        Unsafe.Write(dstPtr + _vectorSpan2, Unsafe.Read<Vector<byte>>(srcPtr + _vectorSpan2));
+                        Unsafe.Write(dstPtr + _vectorSpan3, Unsafe.Read<Vector<byte>>(srcPtr + _vectorSpan3));
+                        if (count == _vectorSpan4) return;
+                        count -= _vectorSpan4;
+                        srcPtr += _vectorSpan4;
+                        dstPtr += _vectorSpan4;
+                    }
+                    if (count >= _vectorSpan2)
+                    {
+                        Unsafe.Write(dstPtr, Unsafe.Read<Vector<byte>>(srcPtr));
+                        Unsafe.Write(dstPtr + _vectorSpan, Unsafe.Read<Vector<byte>>(srcPtr + _vectorSpan));
+                        if (count == _vectorSpan2) return;
+                        count -= _vectorSpan2;
+                        srcPtr += _vectorSpan4;
+                        dstPtr += _vectorSpan4;
+                    }
+                    if (count >= _vectorSpan)
+                    {
+                        Unsafe.Write(dstPtr, Unsafe.Read<Vector<byte>>(srcPtr));
+                        if (count == _vectorSpan) return;
+                        count -= _vectorSpan;
+                        srcPtr += _vectorSpan4;
+                        dstPtr += _vectorSpan4;
+                    }
+                    if (count > 0)
+                    {
+                            var pSrc = srcPtr;
+                            var dSrc = dstPtr;
+
+                            if (count >= _longSpan)
+                            {
+                                var lpSrc = (long*)pSrc;
+                                var ldSrc = (long*)dSrc;
+
+                                if (count < _longSpan2)
+                                {
+                                    count -= _longSpan;
+                                    pSrc += _longSpan;
+                                    dSrc += _longSpan;
+                                    *ldSrc = *lpSrc;
+                                }
+                                else if (count < _longSpan3)
+                                {
+                                    count -= _longSpan2;
+                                    pSrc += _longSpan2;
+                                    dSrc += _longSpan2;
+                                    *ldSrc = *lpSrc;
+                                    *(ldSrc + 1) = *(lpSrc + 1);
+                                }
+                                else
+                                {
+                                    count -= _longSpan3;
+                                    pSrc += _longSpan3;
+                                    dSrc += _longSpan3;
+                                    *ldSrc = *lpSrc;
+                                    *(ldSrc + 1) = *(lpSrc + 1);
+                                    *(ldSrc + 2) = *(lpSrc + 2);
+                                }
+                            }
+                            if (count >= _intSpan)
+                            {
+                                var ipSrc = (int*)pSrc;
+                                var idSrc = (int*)dSrc;
+                                count -= _intSpan;
+                                pSrc += _intSpan;
+                                dSrc += _intSpan;
+                                *idSrc = *ipSrc;
+                            }
+                            while (count > 0)
+                            {
+                                count--;
+                                *dSrc = *pSrc;
+                                dSrc += 1;
+                                pSrc += 1;
+                            }
+                        }
+#if !DEBUG
+                }
+                else
+                {
+                    Unsafe.CopyBlock(dstPtr, srcPtr, (uint)count);
+                }
+#endif
+            }
+        }
+    }
+}
