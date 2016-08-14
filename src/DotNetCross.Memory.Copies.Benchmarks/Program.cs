3138e6f1eaf30c14e2aaa6eb03279d7ecb5c5b40
ntr <ntr@ihfood.dk>
2016-08-14 11:23:42 +0200

    Add bytes to copy generator, new bytes to copy list with more details in small and medium range

--- src/DotNetCross.Memory.Copies.Benchmarks/Program.cs
+++ src/DotNetCross.Memory.Copies.Benchmarks/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using BenchmarkDotNet.Running;
 
 namespace DotNetCross.Memory.Copies.Benchmarks
@@ -8,6 +9,9 @@ namespace DotNetCross.Memory.Copies.Benchmarks
     {
         static void Main(string[] args)
         {
+            //OutputBytesToCopyList();
+            //return;
+
             var sw = new Stopwatch();
             sw.Start();
 
@@ -18,6 +22,29 @@ namespace DotNetCross.Memory.Copies.Benchmarks
             var message = $"Benchmark time: {elapsed} ms";
             Trace.WriteLine(message);
             Console.WriteLine(message);
+        }
+
+        static void OutputBytesToCopyList()
+        {
+            var bytesToCopyList = GenerateBytesToCopyList();
+            var paramsText = string.Join(",", bytesToCopyList);
+            Trace.WriteLine(paramsText);
+            Console.WriteLine(paramsText);
+        }
+
+        static int[] GenerateBytesToCopyList()
+        {
+            var tiny = Enumerable.Range(0, 96).ToArray();
+            
+            var small = Enumerable.Range(3, 28).Select(i => i * 32).ToArray();
+            var smallExtra = small.Select(i => i + 7).Concat(small.Select(i => i + 17)).Concat(small.Select(i => i + 26)).ToArray();
+
+            var medium = Enumerable.Range(4, 32).Select(i => i * 256).ToArray();
+            var mediumExtra = medium.Select(i => i + 62).Concat(medium.Select(i => i + 131)).Concat(medium.Select(i => i + 199)).ToArray();
+
+            var all = tiny.Concat(small).Concat(smallExtra).Concat(medium).Concat(mediumExtra).Distinct().ToArray();
+            Array.Sort(all); // Sort so they are ordered
+            return all;
         }
     }
 }
